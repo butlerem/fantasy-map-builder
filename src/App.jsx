914a5c479a5000ae8aafa79539f8e409a86b2dae
butlerem <butlerem01@gmail.com>
2025-03-16 01:57:37 -0400

    CSS flexbox, import tiles

--- src/App.jsx
+++ src/App.jsx
@@ -15,27 +15,24 @@ function App() {
   return (
     <div className="game-screen">
       <h1 className="game-title">Map Builder</h1>
-
-      {/* Centered canvas container */}
-      <div className="canvas-container">
-        <MapCanvas
-          activeLayer={activeLayer}
-          selectedTile={selectedTiles[activeLayer]}
-        />
+      <div className="main-content">
+        <div className="canvas-container">
+          <MapCanvas
+            activeLayer={activeLayer}
+            selectedTile={selectedTiles[activeLayer]}
+          />
+        </div>
+        <div className="tile-palette-container">
+          <TilePaletteMenu
+            activeLayer={activeLayer}
+            setActiveLayer={setActiveLayer}
+            selectedTiles={selectedTiles}
+            setSelectedTiles={setSelectedTiles}
+          />
+        </div>
       </div>
-
       <div className="toolbar-container">
         <Toolbar />
-      </div>
-
-      {/* Tile palette menu overlay on the right side */}
-      <div className="tile-palette-container">
-        <TilePaletteMenu
-          activeLayer={activeLayer}
-          setActiveLayer={setActiveLayer}
-          selectedTiles={selectedTiles}
-          setSelectedTiles={setSelectedTiles}
-        />
       </div>
     </div>
   );
